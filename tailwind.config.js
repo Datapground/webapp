38f0d99ddf65f81901e8d406b9e4901d153a9c37
sahal imran <sahalimran786@gmail.com>
2025-02-04 15:54:44 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['var(--font-primary)'],
+      },
+      colors: {},
+    },
+  },
+  plugins: [],
+};
