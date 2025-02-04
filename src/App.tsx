f0c1fd2969f43f89b9ec0c20eced911a8d088d41
sahal imran <sahalimran786@gmail.com>
2025-02-04 17:52:35 +0500

    fonts added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<></>} />
+      <Route path="/auth/login" element={<Login />} />
     </Routes>
   );
 };
---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = object;
+
+const Login: React.FC<Props> = () => {
+  return <div className="text-black font-primary">Login</div>;
+};
+
+export default Login;
