459363b96d7cfb726f1e13a1f94e9bce97da0891
sahal imran <sahalimran786@gmail.com>
2025-02-05 12:31:57 +0500

    login bg

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,22 @@ import React from 'react';
 type Props = object;
 
 const Login: React.FC<Props> = () => {
-  return <div className="text-black font-primary">Login</div>;
+  return (
+    <section className="bg-white min-h-screen relative overflow-hidden">
+      {/* robot */}
+      <img
+        src="/robot.png"
+        alt="robot"
+        className="object-contain absolute h-full -left-[15%] pointer-events-none"
+      />
+      {/* Content box */}
+      <section className="absolute left-0 top-0 w-full h-full bg-login p-16">
+        <div className="w-full h-full bg-white rounded-[40px] p-16">
+          {/* Your content here */}
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default Login;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
         primary: ['var(--font-primary)'],
       },
       colors: {},
+      backgroundImage: {
+        login:
+          'linear-gradient(90deg, rgba(46, 225, 226, 0.4) -3.75%, rgba(73, 205, 254, 0.4) 27.32%, rgba(106, 89, 193, 0.4) 73.65%, rgba(149, 65, 163, 0.4) 109.25%)',
+      },
     },
   },
   plugins: [],
