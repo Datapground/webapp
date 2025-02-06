044f47070c4b62df8f27891747838d64a48ffce6
ASHRAF SARWAR <22-cs-19@students.uettaxila.edu.pk>
2025-02-07 02:13:46 +0500

    sidenavigation updated

--- src/components/SideNavigation.tsx
+++ src/components/SideNavigation.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Lottie from 'lottie-react';
 import logoAnimation from '../constants/LogoAnimation.json';
 import { Link, useLocation } from 'react-router-dom';
@@ -24,9 +24,15 @@ const navItems = [
   { path: '/blueprints', label: 'Blueprints', Icon: BluePrintIcon },
   { path: '/workflows', label: 'Workflows', Icon: WorkFlowsIcon },
   { path: '/connections', label: 'Connections', Icon: Connections },
+];
+
+const tools = [
   { path: '/generator', label: 'The Generator', Icon: GeneratorIcon },
   { path: '/predictor', label: 'The Predictor', Icon: PredictorIcon },
   { path: '/extender', label: 'The Extender', Icon: ExtenderIcon },
+];
+
+const accounts = [
   { path: '/apikeys', label: 'API Keys', Icon: APIKeyIcon },
   { path: '/invites', label: 'Invites', Icon: InviteIcon },
   { path: '/usage', label: 'Usage', Icon: UsageIcon },
@@ -35,43 +41,152 @@ const navItems = [
 
 const SideNavigation = () => {
   const location = useLocation();
+  const path = location.pathname;
+
+  const [activeColor, setActiveColor] = React.useState<string>(''); // Default background
+
+  useEffect(() => {
+    if (path === '/generator') {
+      setActiveColor('generator');
+    } else if (path === '/predictor') {
+      setActiveColor('predictor');
+    } else if (path === '/extender') {
+      setActiveColor('extender');
+    } else {
+      setActiveColor('');
+    }
+  }, [path]);
 
   return (
-    <nav className="w-64 h-full rounded-[40px] border-none bg-[#E3ECFF] pb-4 ">
+    <nav
+      className={`w-64 flex flex-col justify-between items-center rounded-[40px] bg-side-navigation pb-4 `}
+    >
       {/* Logo */}
-      <div className="p-4">
-        <Lottie
-          animationData={logoAnimation}
-          loop
-          className="h-[40px] w-[190px] mx-auto"
-        />
+      <div className="w-full relative z-50">
+        <div className="p-4">
+          <Lottie
+            animationData={logoAnimation}
+            loop
+            className="h-[40px] w-[190px] mx-auto"
+          />
+        </div>
+
+        {/* NavItems */}
+        <div className="w-full pl-8 z-50">
+          {navItems.map(({ path, label, Icon }, index) => {
+            const isActive = location.pathname === path;
+            return (
+              <div key={path} className="relative">
+                {/* Item Above */}
+                {navItems.length - index >= 0 && (
+                  <div className="absolute top-full w-full z-40"></div>
+                )}
+
+                <div
+                  key={path}
+                  className={`relative ${isActive ? 'ml-[-4px] my-[-24px] vertical-rounded-tab -z-40' : 'z-30'}`}
+                >
+                  <Link
+                    to={path}
+                    className={`flex items-center py-2 gap-3 my-[5px] rounded-l-[30px] w-full text-sm text-[#414042] px-4 ${
+                      isActive
+                        ? `bg-gradient-to-r from-default to bg-white text-white`
+                        : ''
+                    }`}
+                  >
+                    <Icon
+                      className={`w-[16px] h-[16px] stroke-[#414042] ${isActive ? 'stroke-white' : ''}`}
+                    />
+                    {label}
+                  </Link>
+                </div>
+
+                {index < navItems.length - 1 && (
+                  <div className="absolute top-full w-full z-40"></div>
+                )}
+              </div>
+            );
+          })}
+
+          <div className="line h-[1px] w-[80%] bg-gradient-to-r from-blue-600 to-transparent my-4"></div>
+
+          {tools.map(({ path, label, Icon }, index) => {
+            const isActive = location.pathname === path;
+
+            const gradientClass = activeColor
+              ? `from-${path.substring(1)} to-white`
+              : 'from-default to-white';
+
+            return (
+              <div key={path}>
+                {/* Item Above */}
+                {tools.length - index >= 0 && (
+                  <div className="absolute top-full w-full z-40"></div>
+                )}
+
+                <div
+                  key={path}
+                  className={`relative ${isActive ? 'ml-[-4px] my-[-24px] vertical-rounded-tab -z-50' : 'z-30'}`}
+                >
+                  <Link
+                    to={path}
+                    className={`flex items-center py-2 gap-3 my-[5px] rounded-l-[30px] w-full text-sm text-[#414042] px-4 ${isActive ? `bg-gradient-to-r ${gradientClass} text-white` : ''}`}
+                  >
+                    <Icon
+                      className={`w-[16px] h-[16px] stroke-[#414042] ${isActive ? 'stroke-white' : ''}`}
+                    />
+                    {label}
+                  </Link>
+                </div>
+
+                {index < tools.length - 1 && (
+                  <div className="absolute top-full w-full z-40"></div>
+                )}
+              </div>
+            );
+          })}
+
+          <div className="line h-[1px] w-[80%] bg-gradient-to-r from-blue-600 to-transparent my-4"></div>
+
+          {/* AccountsItems */}
+          {accounts.map(({ path, label, Icon }, index) => {
+            const isActive = location.pathname === path;
+            return (
+              <div key={path} className="relative">
+                {/* Item Above */}
+                {navItems.length - index >= 0 && (
+                  <div className="absolute top-full w-full z-40"></div>
+                )}
+
+                <div
+                  key={path}
+                  className={`relative ${isActive ? 'ml-[-4px] my-[-24px] vertical-rounded-tab -z-50' : 'z-30'}`}
+                >
+                  <Link
+                    to={path}
+                    className={`flex items-center py-2 gap-3 my-[5px] rounded-l-[30px] w-full text-sm text-[#414042] px-4 ${
+                      isActive
+                        ? `bg-gradient-to-r from-default to bg-white text-white`
+                        : ''
+                    }`}
+                  >
+                    <Icon
+                      className={`w-[16px] h-[16px] stroke-[#414042] ${isActive ? 'stroke-white' : ''}`}
+                    />
+                    {label}
+                  </Link>
+                </div>
+
+                {index < navItems.length - 1 && (
+                  <div className="absolute top-full w-full z-40"></div>
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
-      {/* NavItems */}
-      <div className=""> </div>
-      <div className="pl-8">
-        {navItems.map(({ path, label, Icon }) => {
-          const isActive = location.pathname === path;
-          return (
-            <div
-              key={path}
-              className={
-                isActive ? 'ml-[-4px] my-[-22px] vertical-rounded-tab' : ''
-              }
-            >
-              <Link
-                to={path}
-                className={`flex items-center py-2 gap-3 my-[5px] rounded-l-[30px] w-full text-sm text-[#414042] px-4 ${
-                  isActive ? 'bg-gradient-to-r from-pink-500 to bg-white' : ''
-                }`}
-              >
-                <Icon className="w-[16px] h-[16px] stroke-[#414042]" />
-                {label}
-              </Link>
-            </div>
-          );
-        })}
-        {/* <div className="line h-[1px] w-[80%] bg-gradient-to-r from-blue-600 to-transparent my-2"></div> */}
-      </div>
+
+      {/* Settings */}
       <div className="mt-8 mx-auto bg-white text-center text-sm rounded-[30px] w-[40%]">
         <button className="py-2 px-4 flex items-center gap-[5px] text-center text-[#414042]">
           <SettingsIcon className="w-[16px] h-[16px] stroke-[#414042]" />{' '}
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import SideNavigation from '../components/SideNavigation';
 
 const RootLayout = () => {
+  const location = useLocation();
+  const [bgColor, setBgColor] = useState<string>('generator');
+  useEffect(() => {
+    const path = location.pathname;
+
+    if (path === '/generator' || path === '/extender') {
+      setBgColor('bg-extenderBackground');
+    } else if (path === '/predictor') {
+      setBgColor('bg-predictorBackground');
+    } else {
+      setBgColor('bg-white');
+    }
+  }, [location]);
   return (
-    <div className="flex h-screen">
+    <div className={`min-h-screen flex ${bgColor} `}>
       <SideNavigation />
-      <div className="flex-1 p-6">
+      <div className={`flex-1 p-6`}>
         <Outlet />
       </div>
     </div>
